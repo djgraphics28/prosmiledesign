4cb7da2c80d84fdede00de4a435d28c0b5761886
Darz <54253370+djgraphics28@users.noreply.github.com>
2025-04-29 23:15:42 +0800

    update build

---
+++ public/build/assets/input-error-DClpsOaj.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Bbah1jDA.js";import{c as o}from"./button-BWBiPmoH.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
